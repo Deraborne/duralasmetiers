a86d91c3e7f8d435cdbbb765e165b7307dfe8668
Delta-Delete <oktineta@gmail.com>
2026-04-16 03:39:33 +0200

    Create mentions-data.js

---
+++ mentions-data.js
@@ -0,0 +1,8 @@
+window.DURALAS_MEMBERS = {
+  "Nymeris": "https://www.lemondededuralas.org/u896",
+  "Carmina Von Darken": "https://www.lemondededuralas.org/u5678",
+  "Gealbhan": "https://www.lemondededuralas.org/u91011",
+  "Thul Khaen": "https://www.lemondededuralas.org/u1213",
+  "Týrrvan Skuldvörgr": "https://www.lemondededuralas.org/u1415",
+  "Azariël": "https://www.lemondededuralas.org/u1617"
+};
